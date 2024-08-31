ba9852809fae29c2c085af5129efd60ca8763477
Om Bodkhe <ombodkhe2005@gmail.com>
2024-08-31 22:31:09 +0530

    Area Of Circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java .util.*;
+public class AreaOfCircle {
+    public static void main(String[] args){
+        Scanner s1= new Scanner(System.in);
+        System.out.println("Enter Radius Of Cirlce");
+        float num1= s1.nextFloat();
+        double area=num1*3.14;
+        System.out.println("Area Of The Circle For Radius"+ num1 +"is "+" "+area);
+
+}
+}
